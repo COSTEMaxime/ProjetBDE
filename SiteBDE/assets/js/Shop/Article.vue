935958bc915be4481add5a879333d97965b1e326
Unknown <emilien.pons@viacesi.fr>
2018-04-12 11:05:24 +0200

    Ajout toutes les vues (incomplétés)

---
+++ SiteBDE/assets/js/Shop/Article.vue
@@ -0,0 +1,30 @@
+<!-- Le fichier constitue le composant principal, englobant tous les autres composants de l'application et dans lequel
+il faut importer tous les composants nécessaires !-->
+
+<script>
+    import Header from '../components/Header';
+    import Footer from '../components/Footer';
+
+    export default {
+        name: 'register',
+        components: {
+            'appHeader' : Header,
+            'appFooter' : Footer
+        }
+    };
+
+</script>
+
+
+<template>
+  <div id="register">
+    <appHeader></appHeader>
+      <!-- Image + Texte décallage -->
+      <div class="col-md-12 main">
+          <p><img src="/Images/Logo_BDE.png" alt=""></p>
+      </div>
+      <appFooter></appFooter>
+  </div>
+</template>
+
+
--- SiteBDE/assets/js/main.js
+++ SiteBDE/assets/js/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App'
+import Register from './UserGestion/Register'
 
 new Vue({
   el: '#app',
@@ -7,3 +8,8 @@ new Vue({
   components: { App }
 });
 
+new Vue({
+    el: '#register',
+    template: '<Register/>',
+    components: { Register }
+});
