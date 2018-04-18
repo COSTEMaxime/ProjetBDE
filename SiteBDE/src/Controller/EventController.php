fa37c8a8d528ddb6c3a5f2abde8230e5efcd0546
COSTEMaxime <maxime.coste2@hotmail.fr>
2018-04-18 11:49:01 +0200

    Added the AddEventFormClass

--- SiteBDE/src/Controller/EventController.php
+++ SiteBDE/src/Controller/EventController.php
@@ -40,4 +40,69 @@ class EventController extends Controller
             'controller_name' => 'EventController',
         ]);
     }
+
+    /**
+     * @Route("/events/add", name="addEvent")
+     */
+    public function addEvent()
+    {
+        /**
+        $task = new AddEventForm();
+        $form = $this->createFormBuilder($task)
+            ->add('titre', TextType::class)
+            ->add('description', TextType::class)
+            ->add('image', FileType::class)
+            ->add('submit', SubmitType::class, array('label' => 'Submit'))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isSubmitted())
+        {
+            $data = $form->getData();
+
+            //File gestion
+            $file = $data->getImage();
+            /** @var File $file */
+            /**$fileName = $this->generateUniqueFileName().'.'.$file->guessExtension();
+            $file->move(
+                'images/',
+                $fileName
+            );
+
+            $manager = $this->getDoctrine()->getManager();
+
+            $image = new PhotoEntity();
+            $image->setIsFlagged(false);
+            $image->setNblike(0);
+            $image->setPath($fileName);
+            $image->setIDuser(-1);
+            //TODO
+            //$image->setIDuser(getCurrentUserID());
+
+            $manager->persist($image);
+            $manager->flush();
+
+            //Add the idea
+            $idea = new ActiviteEntity();
+            $idea->setTitre($data->getTitre());
+            $idea->setContenu($data->getDescription());
+            $idea->setIDphoto($this->getLastImageID());
+            $idea->setNbLike(0);
+            $idea->setNbDislike(0);
+            $idea->setIDuser(-1);
+            //TODO
+            //$idea->setIDuser(getCurrentUser());
+
+            $manager->persist($idea);
+            $manager->flush();
+
+            return $this->redirectToRoute('ideas');
+        }
+             * */
+        return $this->render('Events/event_add.html.twig', [
+            'controller_name' => 'EventController',
+        ]);
+
+    }
 }
