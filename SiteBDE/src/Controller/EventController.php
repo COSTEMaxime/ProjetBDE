d332b55657807ebcd69a2bb8405ced5d382333eb
Unknown <emilien.pons@viacesi.fr>
2018-04-19 10:44:47 +0200

    Transformation boîte à idées vers Event Contrôleur + TWIG
    Done mais on ne peut pas pré-charger un file

--- SiteBDE/src/Controller/EventController.php
+++ SiteBDE/src/Controller/EventController.php
@@ -24,6 +24,85 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class EventController extends Controller
 {
+    /**
+     * @Route("/events/add", name="addEvent")
+     */
+    public function addEvent(Request $request)
+    {
+        $session = new Session();
+
+        $task = new AddEventForm();
+        $form = $this->createFormBuilder($task)
+            ->add('titre', TextType::class)
+            ->add('description', TextareaType::class)
+            ->add('image', FileType::class, array('label' => 'Image','attr'=> array('name'=>'img','onchange'=>"readURL(this)")))
+            ->add('date', DateType::class,array('label' => 'Image','attr'=> array('id'=>'datepicker')), [
+                'widget' => 'single_text'
+            ])
+            ->add('recurrence', ChoiceType::class, [
+                'choices' => [
+                    'Aucune' => '0',
+                    'Journalière' => '1',
+                    'Hebdomadaire' => '2',
+                    'Annuelle' => '3',
+                ]
+            ])
+            ->add('price', IntegerType::class)
+            ->add('submit', SubmitType::class, array('label' => "Créer l'évènement",'attr'=> array('class'=>'btn btn-primary')))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isSubmitted())
+        {
+            $data = $form->getData();
+
+            //File gestion
+            $file = $data->getImage();
+            /** @var File $file */
+            $fileName = $this->generateUniqueFileName().'.'.$file->guessExtension();
+            $file->move(
+                'Uploads/',
+                $fileName
+            );
+
+            $manager = $this->getDoctrine()->getManager();
+
+            $image = new PhotoEntity();
+            $image->setIsFlagged(false);
+            $image->setNblike(0);
+            $image->setPath($fileName);
+            $image->setIDuser(-1);
+            //TODO
+            //$image->setIDuser(getCurrentUserID());
+
+            $manager->persist($image);
+            $manager->flush();
+
+            //Add the idea
+            $idea = new ManifestationEntity();
+            $idea->setTitre($data->getTitre());
+            $idea->setContenu($data->getDescription());
+            $idea->setDateManifestation($data->getDate());
+            $idea->setIDActivite(-1);
+            $idea->setImage($fileName);
+            $idea->setTypeRecurrence($data->getRecurrence());
+            $idea->setPrix($data->getPrice());
+            $idea->setIsFlagged(false);
+            $idea->setIDuser(-1);
+            //TODO
+            //$idea->setIDuser(getCurrentUser());
+
+            $manager->persist($idea);
+            $manager->flush();
+
+            return $this->redirectToRoute('events');
+        }
+
+        return $this->render('Events/event_add.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
 
     /**
      * @Route("/events/{slug}/toPDF", name="downloadEventPDF", methods={"GET"})
@@ -127,85 +206,7 @@ class EventController extends Controller
         ]);
     }
 
-    /**
-     * @Route("/events/add", name="addEvent")
-     */
-    public function addEvent(Request $request)
-    {
-        $session = new Session();
 
-        $task = new AddEventForm();
-        $form = $this->createFormBuilder($task)
-            ->add('titre', TextType::class)
-            ->add('description', TextareaType::class)
-            ->add('image', FileType::class, array('label' => 'Image','attr'=> array('name'=>'img','onchange'=>"readURL(this)")))
-            ->add('date', DateType::class,array('label' => 'Image','attr'=> array('id'=>'datepicker')), [
-                'widget' => 'single_text'
-            ])
-            ->add('recurrence', ChoiceType::class, [
-                'choices' => [
-                    'Aucune' => '0',
-                    'Journalière' => '1',
-                    'Hebdomadaire' => '2',
-                    'Annuelle' => '3',
-                ]
-            ])
-            ->add('price', IntegerType::class)
-            ->add('submit', SubmitType::class, array('label' => "Créer l'évènement",'attr'=> array('class'=>'btn btn-primary')))
-            ->getForm();
-
-        $form->handleRequest($request);
-
-        if($form->isSubmitted() && $form->isSubmitted())
-        {
-            $data = $form->getData();
-
-            //File gestion
-            $file = $data->getImage();
-            /** @var File $file */
-            $fileName = $this->generateUniqueFileName().'.'.$file->guessExtension();
-            $file->move(
-                'Uploads/',
-                $fileName
-            );
-
-            $manager = $this->getDoctrine()->getManager();
-
-            $image = new PhotoEntity();
-            $image->setIsFlagged(false);
-            $image->setNblike(0);
-            $image->setPath($fileName);
-            $image->setIDuser(-1);
-            //TODO
-            //$image->setIDuser(getCurrentUserID());
-
-            $manager->persist($image);
-            $manager->flush();
-
-            //Add the idea
-            $idea = new ManifestationEntity();
-            $idea->setTitre($data->getTitre());
-            $idea->setContenu($data->getDescription());
-            $idea->setDateManifestation($data->getDate());
-            $idea->setIDActivite(-1);
-            $idea->setImage($fileName);
-            $idea->setTypeRecurrence($data->getRecurrence());
-            $idea->setPrix($data->getPrice());
-            $idea->setIsFlagged(false);
-            $idea->setIDuser(-1);
-            //TODO
-            //$idea->setIDuser(getCurrentUser());
-
-            $manager->persist($idea);
-            $manager->flush();
-
-            return $this->redirectToRoute('events');
-        }
-
-        return $this->render('Events/event_add.html.twig', [
-            'form' => $form->createView()
-        ]);
-    }
 
     private function generateUniqueFileName()
     {
